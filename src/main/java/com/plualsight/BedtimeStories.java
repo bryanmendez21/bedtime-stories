59f43efd165dc4ac2f3128feb6f29e61e7b922cf
bryan <bryanmendez21@icloud.com>
2026-04-21 11:01:58 -0400

    created a do while incase users want to read another story

--- src/main/java/com/plualsight/BedtimeStories.java
+++ src/main/java/com/plualsight/BedtimeStories.java
@@ -5,13 +5,26 @@ import java.util.Scanner;
 public class BedtimeStories {
     public static void main(String[] args) {
         Scanner userInput = new Scanner(System.in);
+        String nextStory;
 
-        System.out.println("----------Welcome Choose From The Stories----------");
-        System.out.println("(1) goldilocks");
-        System.out.println("(2) hansel and gretel");
-        System.out.println("(3) mary had a little lamb");
-        System.out.print("Pick Using A Numerical Value: ");
-        int userChoice = userInput.nextInt();
+        do {
+            System.out.println("----------Welcome Choose From The Stories----------");
+            System.out.println("(1) goldilocks");
+            System.out.println("(2) hansel and gretel");
+            System.out.println("(3) mary had a little lamb");
+            System.out.println("(0) Exit");
+            System.out.print("Pick Using A Numerical Value: ");
+            int userChoice = userInput.nextInt();
+            userInput.nextLine();
+
+
+            System.out.print("Want to read another story? (yes/no): ");
+            nextStory = userInput.nextLine();
+
+        } while(nextStory.equalsIgnoreCase("yes"));
+
+        System.out.println("Good Bye");
+        userInput.close();
 
 
     }
