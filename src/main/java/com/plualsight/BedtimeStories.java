b0db8910e0e970f01d5851acc23e74c20c0cccd9
bryan <bryanmendez21@icloud.com>
2026-04-21 10:47:06 -0400

    created a little menu so users can pick a story

--- src/main/java/com/plualsight/BedtimeStories.java
+++ src/main/java/com/plualsight/BedtimeStories.java
@@ -1,4 +1,18 @@
 package com.plualsight;
 
+import java.util.Scanner;
+
 public class BedtimeStories {
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.println("----------Welcome Choose From The Stories----------");
+        System.out.println("(1) goldilocks");
+        System.out.println("(2) hansel and gretel");
+        System.out.println("(3) mary had a little lamb");
+        System.out.print("Pick Using A Numerical Value: ");
+        int userChoice = userInput.nextInt();
+
+
+    }
 }
